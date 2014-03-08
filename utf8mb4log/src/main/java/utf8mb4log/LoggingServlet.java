85f61b2ef406ce02353654065ee02e9b55d8a294
lbtc-xxx <lbtc-xxx@bridge9.sakura.ne.jp>
2014-03-08 17:34:11 +0900

    utf8mb4 logging through slf4j

---
+++ utf8mb4log/src/main/java/utf8mb4log/LoggingServlet.java
@@ -0,0 +1,22 @@
+package utf8mb4log;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/")
+public class LoggingServlet extends HttpServlet {
+	private static final Logger log = LoggerFactory.getLogger(LoggingServlet.class);
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.info("𥹖𥹖𥹖");
+	}
+}
